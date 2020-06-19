7faf485b5aef5c7abb24b61034715745ac79a230
LouisWelstead <louis.welstead@googlemail.com>
2020-06-19 23:12:15 +0100

    header

--- src/App.js
+++ src/App.js
@@ -7,14 +7,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Louis Welstead
-          dfgdfgdfg
-          dfgdfg
-        </p>
-        <p>
-          WEBSITE
-        </p>
+       <h1>
+         Louis Welstead
+       </h1>
         <a
           className="App-link"
           href="https://reactjs.org"
